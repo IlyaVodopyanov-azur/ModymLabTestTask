d66f14c014bf59601467d13e13bccc4ba118753a
IlyaVodop <Ilyavodop1@gmail.com>
2020-12-29 03:03:58 +0300

    Spawn system

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PlatformPrefab;
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            var platform = Instantiate(PlatformPrefab);
+            platform.transform.position = new Vector3(i, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public float maxSpeed;
     public float speed;
-
+    private float _timeDown;
+    private float _timeUp;
     private Rigidbody rb;
 
     void Start()
@@ -16,10 +17,31 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _timeDown = Time.time;
 
-        Vector3 movement = new Vector3(h * speed, rb.velocity.y , v * speed);
-        rb.AddForce(movement);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _timeUp = Time.time;
+            if (_timeUp - _timeDown > 0.5f)
+            {
+                rb.AddForce(0, 10000, 0);
+                Debug.LogError("СИЛЬНЫЙ ПРЫЖОК");
+            }
+            if (_timeUp - _timeDown < 0.5f)
+            {
+                rb.AddForce(0, 1000, 0);
+                Debug.LogError("СЛАБЫЙ ПРЫЖОК");
+            }
+        }
+
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(speed, rb.velocity.y, speed);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+       rb.AddForce(movement);
     }
 }
